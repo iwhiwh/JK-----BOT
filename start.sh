6cf3311377870c748aa882a869d9875fa21f51c3
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2023-09-20 16:19:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeamHMT/Auto-search-tamil-bot.git /Auto-search-tamil-bot
+  git clone https://github.com/jkdeveloper01/MAKEAUTO.git /MAKEAUTO
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /MAKEAUTO
 fi
-cd /Auto-search-tamil-bot
+cd /MAKEAUTO
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
